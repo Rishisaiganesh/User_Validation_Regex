5746a3cdde8814c921b152ab218ecc91b2471045
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-19 11:41:13 +0530

    [Rishi]ValidatingName

---
+++ _Regex_Validation/_Regex_Validation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _Regex_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regex");
+            Validation value = new Validation();
+            value.ValidatingName("Rishisai");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _Regex_Validation/_Regex_Validation/Validation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _Regex_Validation
+{
+    public class Validation
+    {
+        public void ValidatingName(string Name)
+        {
+            string stringValidation = "^[A-Z]{1}[A-Za-z]{3,10}";
+            if (Regex.IsMatch(Name, stringValidation))
+            {
+                Console.WriteLine(Name + "Its Validated");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Given valid Name");
+            }
+
+        }
+    }
+}
