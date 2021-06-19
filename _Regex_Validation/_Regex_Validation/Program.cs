12413126a645091fd3f4694ecd448cefb6cea001
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-19 12:02:26 +0530

    [Rishi]ValidatingMail

--- _Regex_Validation/_Regex_Validation/Program.cs
+++ _Regex_Validation/_Regex_Validation/Program.cs
@@ -6,14 +6,18 @@ namespace _Regex_Validation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Regex");
+            //Console.WriteLine("Regex");
             Validation value = new Validation();
-            Console.WriteLine("enter firstName");
-            string Firstname = Console.ReadLine();
-            value.ValidatingName(Firstname);
-            Console.WriteLine("Enter LastName");
-            string LastName = Console.ReadLine();
-            value.ValidatingName(LastName);
+            //Console.WriteLine("enter firstName");
+            //string Firstname = Console.ReadLine();
+            //value.ValidatingName(Firstname);
+            //Console.WriteLine("Enter LastName");
+            //string LastName = Console.ReadLine();
+            //value.ValidatingName(LastName);
+            //Console.ReadKey();
+            Console.WriteLine("Enter Valid mail id:");
+            string Mail = Console.ReadLine();
+            value.ValidatingMail(Mail);
             Console.ReadKey();
         }
     }
--- _Regex_Validation/_Regex_Validation/Validation.cs
+++ _Regex_Validation/_Regex_Validation/Validation.cs
@@ -7,6 +7,10 @@ namespace _Regex_Validation
 {
     public class Validation
     {
+        /// <summary>
+        /// Validating name Using Regex
+        /// </summary>
+        /// <param name="Name"></param>
         public void ValidatingName(string Name)
         {
             string stringValidation = "^[A-Z]{1}[A-Za-z]{3,20}";
@@ -20,6 +24,23 @@ namespace _Regex_Validation
                 Console.WriteLine("Not Given valid Name");
             }
 
+        }
+        /// <summary>
+        /// Validating Mail ID Using Regex
+        /// </summary>
+        /// <param name="Mail"></param>
+        public void ValidatingMail(string Mail)
+        {
+            string stringmail = "^[A-Z]{1}[A-Za-z]{3,30}[0-9]@gmail(.com|.in)";
+            if(Regex.IsMatch(Mail,stringmail))
+            {
+                Console.WriteLine(Mail + "Its valid Mail Id");
+
+            }
+            else
+            {
+                Console.WriteLine("Its in Valid mailId");
+            }
         }
     }
 }
